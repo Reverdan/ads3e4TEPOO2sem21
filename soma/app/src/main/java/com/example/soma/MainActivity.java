4c2fd98f27311df46f8acc09505807711f58d103
Reverdan Almeida Sparinger <reverdan@gmail.com>
2021-09-02 22:01:01 -0300

    Soma
    Soma com toast e try catch

--- soma/app/src/main/java/com/example/soma/MainActivity.java
+++ soma/app/src/main/java/com/example/soma/MainActivity.java
@@ -3,14 +3,57 @@ package com.example.soma;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
 {
+    EditText edtNumero1;
+    EditText edtNumero2;
+    Button btnSomar;
+    TextView txvResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        inicializarComponentes();
+        eventos();
+    }
+
+    public void inicializarComponentes()
+    {
+        edtNumero1 = findViewById(R.id.edtNumero1);
+        edtNumero2 = findViewById(R.id.edtNumero2);
+        btnSomar = findViewById(R.id.btnSomar);
+        txvResultado = findViewById(R.id.txvResultado);
+    }
+
+    public void eventos()
+    {
+        btnSomar.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                try
+                {
+                    Double num1 = Double.parseDouble(edtNumero1.getText().toString());
+                    Double num2 = Double.parseDouble(edtNumero2.getText().toString());
+                    Double res = num1 + num2;
+                    txvResultado.setText(res.toString());
+                }
+                catch (NumberFormatException e)
+                {
+                    txvResultado.setText("Digite números válidos");
+                    Toast.makeText(getApplicationContext(), "Digite números válidos", Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
     }
 }
