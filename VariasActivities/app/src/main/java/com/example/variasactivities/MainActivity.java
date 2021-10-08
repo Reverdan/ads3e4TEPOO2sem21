4765c2454f39982e65d175b32420303c8da9d15f
Reverdan Almeida Sparinger <reverdan@gmail.com>
2021-10-07 21:48:22 -0300

    Varias activities

--- VariasActivities/app/src/main/java/com/example/variasactivities/MainActivity.java
+++ VariasActivities/app/src/main/java/com/example/variasactivities/MainActivity.java
@@ -1,9 +1,13 @@
 package com.example.variasactivities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -25,6 +29,37 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         inicializarComponentes();
         eventos();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_principal, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item)
+    {
+//        if (item.getItemId() == R.id.itmPrimo)
+//        {
+//            startActivity(intPrimo);
+//        }
+//        if (item.getItemId() == R.id.itmFatorial)
+//        {
+//            startActivity(intFatorial);
+//        }
+        switch (item.getItemId())
+        {
+            case R.id.itmPrimo:
+                startActivity(intPrimo);
+                return true;
+            case R.id.itmFatorial:
+                startActivity(intFatorial);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void inicializarComponentes()
